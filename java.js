8de39399144179a3aa66b347d92a9828a3cfb0d2
Anniick-dev <141575965+Anniick-dev@users.noreply.github.com>
2023-09-10 13:05:37 +0200

    Cleaned up code and starting on timezone introduction

--- java.js
+++ java.js
@@ -13,27 +13,44 @@ updateDate();
 
 setInterval(updateDate, 1000);
 
-// function to search for specific city //
-function searchCity(city) { 
-  let apiKey = "fbef01f4et1b02o0d25c27210a43ef3f";
-  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiURL).then(showWeather);  
-
+//Show selected country in dropdown upon selection //
+function showSelectedCity (event) {
+  if (event.target.value.length > 0) {
+  alert(event.target.value);
   }
 
- // function to search for city //
-function handleSubmit(event) {
-  event.preventDefault(); 
-  let city = document.querySelector("#city-input").value;
-  searchCity(city);
+  if (event.target.value == 'willemstad') {
+    alert ("It is currently ${timeZone} in Willemstad, Curacao!");
+  } 
+
+  if (event.target.value == 'amsterdam') {
+    alert ("It is currently ${timeZone} in Amsterdam, The Netherlands!");
+  }
+
+  if (event.target.value == 'los-angeles') {
+    alert ("It is currently [${timeZone} in Los Angeles, USA!");
+  } 
 }
 
-function searchLocation(position) {
-  let apiKey = "f97e6oa7271ce6b46a866b531489t0f6";
-  let apiURL = `https://api.shecodes.io/weather/v1/current?lon=${position.coords.longitude}&lat=${position.coords.latitude}&key=${apiKey}&units=metric`;
-  
-  axios.get(apiURL).then(showWeather);  
-}
+let cityDropdown = document.querySelector("#city-dropdown");
+cityDropdown.addEventListener('change', showSelectedCity);
+
+// timezone amsterdam //
+let amsterdamTime = moment()
+        .tz("Europe/Amsterdam")
+        .format("dddd, MMMM D, YYYY h:m A");
+
+      let amsterdamElement = document.querySelector("#amsterdam");
+
+      amsterdamElement.innerHTML = amsterdamTime;
+
+// timezone current location //
+let localTimezone = moment.tz.guess();
+      let localElement = document.querySelector("#local");
+      localElement.innerHTML = `Your current time zone is ${localTimezone} and the current time is ${moment().format(
+        "h:m A"
+      )}`;
+
 
   // function for current position //
 function getCurrentLocation(event) {
@@ -41,47 +58,7 @@ function getCurrentLocation(event) {
   navigator.geolocation.getCurrentPosition(searchLocation);
   }
 
-  // showWeather //
-  function showWeather(response) {
-    document.querySelector("#city").innerHTML = response.data.city;
-    let celsiusTemperature = Math.round(response.data.temperature.current);
-    let feelTemp = Math.round(response.data.temperature.feels_like);
-    let descriptionText = response.data.condition.description;
-    document.querySelector("#temperature").innerHTML = Math.round(celsiusTemperature);
-    document.querySelector("#description").innerHTML = `It is a day filled with ${descriptionText} & feels like ${feelTemp}&degC. Enjoy!`;
-    document.querySelector("#hum").innerHTML = response.data.temperature.humidity;
-    document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
-    
-    let iconElement = document.querySelector("#icon");
-    iconElement.setAttribute(
-        "src",
-        `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`);
-    iconElement.setAttribute(
-        "alt", response.data.condition.description);
-    
-    getForecast(response.data.coordinates);
-}
 
-// Udate the date and time //
-function updateDateTime() {
-  let dateElement = document.querySelector("#date");
-  let currentTime = new Date();
-  dateElement.innerHTML = formatDate(currentTime);
-}
-
-updateDateTime();
-
-
-
-// push through search //
-let searchForm = document.querySelector("form");
-searchForm.addEventListener("submit", handleSubmit);
-  
-// find current location //
-let currentLocationButton = document.querySelector("#current-position");
-currentLocationButton.addEventListener("click", getCurrentLocation);
-
-searchCity("New York");
 
 // Get slider to do something //
 function updateTemperatureUnits() {
@@ -139,38 +116,4 @@ function updateTemperatureUnits() {
 
 var unitToggle = document.querySelector(".switch-control-input");
 unitToggle.addEventListener("change", updateTimeUnits);
-
-//Show selected country in dropdown upon selection //
-
-function showSelectedCity (event) {
-  if (event.target.value.length > 0) {
-  alert(event.target.value);
-  }
-
-  if (event.target.value == 'willemstad') {
-    alert ("It is currently [time] in Willemstad, Curacao!");
-  } 
-
-  if (event.target.value == 'amsterdam') {
-    alert ("It is currently [time] in Amsterdam, The Netherlands!");
-  }
-
-  if (event.target.value == 'los-angeles') {
-    alert ("It is currently [time] in Los Angeles, USA!");
-  } 
-}
-
-let cityDropdown = document.querySelector("#city-dropdown");
-cityDropdown.addEventListener('change', showSelectedCity);
-
-// Moments //
-
-let today = moment().format("dddd Do MMMM");
-      let todayElement = document.querySelector("#today");
-      todayElement.innerHTML = today;
-
-      let tomorrowElement = document.querySelector("#tomorrow");
-      
-let tomorrow = moment().add(1, "day").format("dddd Do, MMMM");
-      tomorrowElement.innerHTML = `Tomorrow will be ${tomorrow}`;
 
