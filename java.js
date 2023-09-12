e2e8f903bc723b0535fc421f6940533386008986
Anniick-dev <141575965+Anniick-dev@users.noreply.github.com>
2023-09-12 14:42:43 +0200

    Added local and updateTime function for interval

--- java.js
+++ java.js
@@ -13,6 +13,8 @@ updateDate();
 updateCityTime();
 
 setInterval(updateDate, 1000);
+setInterval(updateCityTime, 1000);
+
 
 // Function to update the time //
 function updateCityTime() {
@@ -72,6 +74,27 @@ function updateCityTime() {
   }
 }
 
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  if (cityTimeZone === "local") {
+    cityTimeZone = moment.tz.guess();
+  }
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
+}
+
 //Show selected city in dropdown upon selection //
   function showDate(event) {
     if (event.target.value.length > 0) {
@@ -94,66 +117,5 @@ function updateCityTime() {
   let selectElement = document.querySelector("#city-dropdown");
   selectElement.addEventListener("change", showDate);
     
-
-
-
-
-
-
-
-// Get slider to do something //
-function updateTemperatureUnits() {
-  let temperatureElement = document.querySelector("#temperature");
-  let unitToggle = document.querySelector(".switch-control-input");
-  let temperatureUnitsElement = document.querySelector(".temperatureUnits");
-  let windElement = document.querySelector("#wind");
-  let windUnitElement = document.querySelector(".windUnit");
-
-  let minTempElement = document.querySelector("#minTemp");
-  let maxTempElement = document.querySelector("#maxTemp");
-
-  if (unitToggle.checked) {
-    // Celsius to Fahrenheit conversion for current temperature
-    let celsiusTemperature = parseFloat(temperatureElement.textContent);
-    let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-    temperatureElement.textContent = Math.round(fahrenheitTemperature);
-    temperatureUnitsElement.innerHTML = "&deg;F";
-
-    // Celsius to Fahrenheit conversion for wind speed
-    let windCelsius = parseFloat(windElement.textContent);
-    let windFahrenheit = windCelsius * 0.621371;
-    windElement.textContent = Math.round(windFahrenheit);
-    windUnitElement.innerHTML = "m/h";
-
-    // Celsius to Fahrenheit conversion for min and max temperatures
-    let minCelsiusTemperature = parseFloat(minTempElement.textContent);
-    let maxCelsiusTemperature = parseFloat(maxTempElement.textContent);
-    let minFahrenheitTemperature = (minCelsiusTemperature * 9) / 5 + 32;
-    let maxFahrenheitTemperature = (maxCelsiusTemperature * 9) / 5 + 32;
-    minTempElement.textContent = Math.round(minFahrenheitTemperature);
-    maxTempElement.textContent = Math.round(maxFahrenheitTemperature);
-  } else {
-    // Fahrenheit to Celsius conversion for current temperature
-    let fahrenheitTemperature = parseFloat(temperatureElement.textContent);
-    let celsiusTemperature = ((fahrenheitTemperature - 32) * 5) / 9;
-    temperatureElement.textContent = Math.round(celsiusTemperature);
-    temperatureUnitsElement.innerHTML = "&deg;C";
-
-    // Fahrenheit to Celsius conversion for wind speed
-    let windFahrenheit = parseFloat(windElement.textContent);
-    let windCelsius = windFahrenheit / 0.621371;
-    windElement.textContent = Math.round(windCelsius);
-    windUnitElement.innerHTML = "km/h";
-
-    // Fahrenheit to Celsius conversion for min and max temperatures
-    let minFahrenheitTemperature = parseFloat(minTempElement.textContent);
-    let maxFahrenheitTemperature = parseFloat(maxTempElement.textContent);
-    let minCelsiusTemperature = ((minFahrenheitTemperature - 32) * 5) / 9;
-    let maxCelsiusTemperature = ((maxFahrenheitTemperature - 32) * 5) / 9;
-    minTempElement.textContent = Math.round(minCelsiusTemperature);
-    maxTempElement.textContent = Math.round(maxCelsiusTemperature);
-  }
-}
-
-var unitToggle = document.querySelector(".switch-control-input");
-unitToggle.addEventListener("change", updateTimeUnits);
+  let citiesSelectElement = document.querySelector("#city");
+  citiesSelectElement.addEventListener("change", updateCity);
